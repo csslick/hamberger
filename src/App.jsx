dfbe6b7588bb2d317c1a5d1d93d109beea07190c
csslick <digitone@naver.com>
2023-08-16 20:57:18 +0900

    재료 제거하기

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,38 @@ import Tomato from './components/tomato'
 import Cheese from './components/Cheese'
 import Cutlet from './components/Cutlet'
 
+// icons
+import { LuBeef } from 'react-icons/lu'
+import { BiCheese } from 'react-icons/bi'
+import { GiTomato } from 'react-icons/gi'
+import { LuSalad } from 'react-icons/lu'
+import { GrPowerReset} from 'react-icons/gr'
+
 function App() {
   const [slices, setSlices] = useState([])
+
   const addSlice = (slice) => {
     setSlices([slice, ...slices,]);
   }
-  console.log(slices);
 
+  const removeSlice = (id) => {
+    console.log(id);
+    const updateSlice = slices.filter((slice, i) => {
+      return i !== id;
+    })
+    setSlices(updateSlice);
+  } 
+  
   return (
     <div className="App">
       <h1>Cooking a Hamburger</h1>
-      <Hamburger slices={slices} />
+      <Hamburger slices={slices} removeSlice={removeSlice} />
       <div className="menu">
-        <button onClick={()=>{addSlice(<Cutlet/>)}}>고기</button>
-        <button onClick={()=>{addSlice(<Cheese/>)}}>치즈</button>
-        <button onClick={()=>{addSlice(<Tomato/>)}}>토마토</button>
-        <button onClick={()=>{addSlice(<Salad/>)}}>샐러드</button>
-        <button onClick={()=>{setSlices([])}} className='btn-reset'>초기화</button>
+        <button onClick={() => { addSlice(<Cutlet />) }}><LuBeef /></button>
+        <button onClick={() => { addSlice(<Cheese />) }}><BiCheese /></button>
+        <button onClick={() => { addSlice(<Tomato />) }}><GiTomato /></button>
+        <button onClick={() => { addSlice(<Salad />) }}><LuSalad /></button>
+        <button onClick={() => { setSlices([]) }} className='btn-reset'><GrPowerReset /></button>
       </div>
     </div>
   )
